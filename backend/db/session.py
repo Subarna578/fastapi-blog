3dc2addf17004d5d5bad1ab1f666f4b5933c4062
slamsal <slamsal@commvault.com>
2025-11-30 13:39:29 +0530

    Added postgres support

---
+++ backend/db/session.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from core.config import settings 
+
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+
+print(SQLALCHEMY_DATABASE_URL)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SESSIONLOCAL = sessionmaker(autoflush=False, autocomiit=False, bind=engine)
+
--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
 from core.config import settings
+from db.session import engine
+from db.base_class import Base
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
 
 
-app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION, summary = "Blog page")
+def start_application():
+    app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION, summary = "Blog page")
+    create_tables()
+    return app
+
+app = start_application()
 
 @app.get("/")
 def hello():
     return {"msg": "Hello FastAPI"}
+
